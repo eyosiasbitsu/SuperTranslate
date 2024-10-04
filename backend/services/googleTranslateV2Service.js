69a416518138bd03d57304a66657e1053640d5c3
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-10-05 00:15:17 +0300

    Cleanup google translate services

--- backend/services/googleTranslateV2Service.js
+++ backend/services/googleTranslateV2Service.js
@@ -2,18 +2,15 @@ const { Translate } = require('@google-cloud/translate').v2;
 const { getLanguageCodeForGoogle } = require('../utils/languageMapperService');
 const dotenv = require('dotenv');
 
-// Load environment variables
 dotenv.config();
 
-// Initialize Google Cloud Translate client
 const translateV2 = new Translate();
 
 const googleTranslateTextV2 = async (text, targetLanguage) => {
   try {
-    // Get language code for Google
+
     const targetLanguageCode = await getLanguageCodeForGoogle(targetLanguage);
 
-    // Perform translation
     const [translation] = await translateV2.translate(text, targetLanguageCode);
     return translation;
 
--- backend/services/googleTranslateV3Service.js
+++ backend/services/googleTranslateV3Service.js
@@ -3,42 +3,35 @@ const path = require('path');
 const { getLanguageCodeForGoogle } = require('../utils/languageMapperService');
 const dotenv = require('dotenv');
 
-// Load environment variables
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
-// Initialize the Translation Service Client
 const translationClientV3 = new TranslationServiceClient();
 const googleProjectId = process.env.GOOGLE_CLOUD_PROJECT_ID || 'supertranslate';
-const location = 'global'; // You can change this to your specific location if needed
+const location = 'global';
 
 /**
- * Translates text using Google Cloud Translate v3 API.
- *
  * @param {string} text - The text to translate.
  * @param {string} targetLanguage - The target language for translation.
  * @returns {Promise<string>} - The translated text.
  */
 const googleTranslateTextV3 = async (text, targetLanguage) => {
   try {
-    // Get the correct language code for Google Translate
+
     const targetLanguageCode = await getLanguageCodeForGoogle(targetLanguage);
 
-    // Prepare the request object
     const request = {
       parent: `projects/${googleProjectId}/locations/${location}`,
       contents: [text],
-      mimeType: 'text/plain', // Could also be 'text/html' if your input is HTML
+      mimeType: 'text/plain',
       targetLanguageCode,
     };
 
-    // Send the request to Google Cloud Translate
     const [response] = await translationClientV3.translateText(request);
     const translatedText = response.translations[0].translatedText;
 
     return translatedText;
 
   } catch (error) {
-    // Log and throw the error for further handling
     console.error('Error in Google Translate v3:', error.message || error);
     throw error;
   }
